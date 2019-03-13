b5fc604e9294aa30aa5cecbc6fbdbf4b4bd74c17
valdislav i <vladoss_19995@mail.ru>
2019-03-13 10:14:17 +0300

    fix menu

--- src/pages/home/components/Header/Header.vue
+++ src/pages/home/components/Header/Header.vue
@@ -5,11 +5,11 @@
         span.header-top__hielo
           | Hielo &nbsp;
         | by TEMPLATED
-      .header-top__menu( @click="isVisible = !isVisible" :class="{close: isVisible}")
+      .header-top__menu( @click="hide" :class="{close: isVisible}")
         .header-top__strip
           span
         | menu
-    nav.header-top__nav( :class="{visible: isVisible}")
+    nav.header-top__nav( :class="{visible: isVisible}" )
       ul.header-top__list
           li.header-top__item(v-for="item of items")
               a.header__link(:href="item.href" @click="isVisible = false") {{ item.text }}
@@ -30,31 +30,18 @@ import './index.sass'
               ]
           }
       },
-      watch: {
-          active: function () {
-              document.body.style.overflow = this.isVisible ? '' : 'hidden'
+      methods: {
+          hide: function () {
+              this.isVisible = !this.isVisible;
+              console.log(this.isVisible);
+              if(this.isVisible === true) {
+                  document.body.classList.add('noscroll');
+              }
+              else {
+                  document.body.classList.remove('noscroll');
+              }
           }
       }
-      // created() {
-      //
-      //     window.onload = function () {
-      //         var aaaa = document.querySelector('.header-top__menu');
-      //
-      //         aaaa.onclick = function () {
-      //             var header = document.querySelector('.header-top__nav');
-      //             var body = document.body;
-      //             if (!header.classList.contains('visible')) {
-      //                 console.log(11111);
-      //                 console.log(body);
-      //                 body.classList.add('no--scroll')
-      //             } else {
-      //                 if (body.classList.contains('no--scroll')) {
-      //                     body.classList.remove('no--scroll');
-      //                 }
-      //             }
-      //         }
-      //     }
-      // }
   }
 
 </script>
